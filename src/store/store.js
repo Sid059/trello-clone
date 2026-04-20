6661aa4a3931a8291670cab92fe397f6f00bd46d
Siddharth <siddharthrai139@gmail.com>
2026-04-20 16:48:17 +0100

    Created store.js and added boards

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import boardsReducer from '../features/boards/boardsSlice';
+//import listsReducer from '../features/lists/listsSlice';    //later to be worked
+
+const rootReducer = combineReducers({
+    boards: boardsReducer,
+    //lists: listsReducer, //later to be worked
+});
+
+const persistConfig = {
+    key: 'trello-root',
+    storage,
+    whitelist: ['boards']       //only boards for now
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducers: {
-        
-    }
+    reducers: persistedReducer,
+    middleware: (getDefaultMiddleware) => (
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'] // Ignore these actions for serializability check
+            }
+        })
+    )
 })
+
+export const persistor = persistStore(store);
